6bc395c7d2fa714b49af856eb99e9574749df942
Dendy2468 <105947041+Dendy2468@users.noreply.github.com>
2022-06-19 01:26:59 +0700

    commit-final2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 alert("Selamat Datang");
+
+function blogText1() {
+    document.getElementById("lanjut1").innerHTML = "Tetapi saya akan tetap terus mencoba karena membuat blog terdengar menarik.";
+  }
+  
+  function blogText2() {
+    document.getElementById("lanjut2").innerHTML = "Bersih-bersih rumahnya berupa menyapu, mengepel, menguras dan membersihkan kamar mandi";
+  }
+
+  function blogText3() {
+    document.getElementById("lanjut3").innerHTML = "Saya sudah mencari ke ind*mart, alf*mart, dan om* tetapi harganya mahal semua.";
+  }
